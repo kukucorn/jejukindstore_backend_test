8a93a3acec04e7a7959e7fd2af57f0d0d186eb72
jungheun9 <jungheun99@naver.com>
2021-04-09 22:45:54 +0900

    Add function save store
    at service, controller

--- demo/src/main/java/com/kukucorn/jejukindstore/service/StoreService.java
+++ demo/src/main/java/com/kukucorn/jejukindstore/service/StoreService.java
@@ -1,8 +1,10 @@
 package com.kukucorn.jejukindstore.service;
 
+import com.kukucorn.jejukindstore.domain.store.Store;
 import com.kukucorn.jejukindstore.domain.store.StoreRepository;
 import com.kukucorn.jejukindstore.web.dto.StoreListResponseDto;
 import com.kukucorn.jejukindstore.web.dto.StoreResponseDto;
+import com.kukucorn.jejukindstore.web.dto.StoreSaveRequestDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -19,5 +21,11 @@ public class StoreService {
     @Transactional
     public List<StoreListResponseDto> findAll() {
         return storeRepository.findAll().stream().map(StoreListResponseDto::new).collect(Collectors.toList());
+    }
+
+    @Transactional
+    public Integer save(StoreSaveRequestDto requestDto) {
+        Store store = storeRepository.save(requestDto.toEntity());
+        return store.getId();
     }
 }
--- demo/src/main/java/com/kukucorn/jejukindstore/web/StoreApiController.java
+++ demo/src/main/java/com/kukucorn/jejukindstore/web/StoreApiController.java
@@ -2,9 +2,13 @@ package com.kukucorn.jejukindstore.web;
 
 import com.kukucorn.jejukindstore.service.StoreService;
 import com.kukucorn.jejukindstore.web.dto.StoreListResponseDto;
+import com.kukucorn.jejukindstore.web.dto.StoreSaveRequestDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.context.annotation.RequestScope;
 
 import java.util.List;
 
@@ -17,5 +21,10 @@ public class StoreApiController {
     @GetMapping("/api/v1/store")
     public List<StoreListResponseDto> findAll() {
         return storeService.findAll();
+    }
+
+    @PostMapping("/api/v1/store")
+    public Integer save(@RequestBody StoreSaveRequestDto requestDto) {
+        return storeService.save(requestDto);
     }
 }
